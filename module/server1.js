7a274104716a9a25b3a918bd25ffaddbedb248bf
ssolanki <sahilsolanki07@gmail.com>
2016-04-01 09:28:28 +0530

    use of connect for hitting url

--- module/server1.js
+++ module/server1.js
@@ -4,19 +4,15 @@ var http = require('http');
 // app wil handle how request will be handled
 var app = connect();
 
-function doFirst(request,response,next){
-	console.log("Bacon");
-	// this are function where u can check like whether have a permission etc.
-	next(); // ask for next proc in stack *which is executing doSecond*
+function profile(request,response){
+	console.log("User requested for profile");
 }
-function doSecond(request,response,next){
-	console.log("Jack");
-	next();
+function forum(request,response){
+	console.log("User requested for forum");
 }
-// middleware
-app.use(doFirst);
-console.log('starting exec of doSecond function...');
-app.use(doSecond);
+app.use('/profile',profile);
+app.use('/forum',forum);
+
 
 http.createServer(app).listen(8888);
 console.log('listening at the port 8888...');
